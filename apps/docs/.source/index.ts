bf69594e1f6973c9362f016b3a1edf09e8108716
Remco Stoeten <remcostoeten@hotmail.com>
2025-02-08 15:34:57 +0100

    fix: proper type for build

---
+++ apps/docs/.source/index.ts
@@ -0,0 +1,6 @@
+// @ts-nocheck -- skip type checking
+import * as docs_doc_1 from "../content/docs/test.mdx?collection=docs&hash=0949aa27abd3c373b3ce53522bb37d28"
+import * as docs_doc_0 from "../content/docs/index.mdx?collection=docs&hash=0949aa27abd3c373b3ce53522bb37d28"
+import { _runtime } from "fumadocs-mdx"
+import * as _source from "../source.config"
+export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","absolutePath":"/home/remcostoeten/PROPAH/ryoa-mono-docs/apps/docs/content/docs/index.mdx"}, data: docs_doc_0 }, { info: {"path":"test.mdx","absolutePath":"/home/remcostoeten/PROPAH/ryoa-mono-docs/apps/docs/content/docs/test.mdx"}, data: docs_doc_1 }], [])
