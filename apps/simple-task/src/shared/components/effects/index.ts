4b331edb9723f6d21262e13a77054c499dec48ce
Remco (macbook) <remcostoeten@hotail.com>
2025-02-14 21:58:18 +0100

    refactor: clean empty app

--- apps/simple-task/src/shared/components/effects/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './loaders/loader-bar'
-export * from './loaders/spinner'
-export * from './text-effect'
--- apps/simple-task/src/shared/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './use-slash-commands'
-export * from './use-mock-user'
-export * from './use-mobile'
